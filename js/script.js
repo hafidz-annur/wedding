23dc1c7de5d8334d6d891504cd71bed0d550421d
annur25 <hafidz.bdt@gmail.com>
2021-01-26 16:42:32 +0700

    wedding

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 $(window).scroll(function () {
     var wScroll = $(this).scrollTop()
 
-    $("#bg1").css({
+    $(".bg1").css({
         'transform': 'translate(0px, ' + wScroll / 20 + '%)',
         'z-index': 1
     })
-    $("#bg2").css({
+    $(".bg2").css({
         'transform': 'translate(0px, ' + wScroll / 40 + '%)',
         'z-index': 1
     })
-    $("#bg3").css({
+    $(".bg3").css({
         'transform': 'translate(0px, ' + wScroll / 60 + '%)',
         'z-index': 1
     })
-    $("#bg4").css({
+    $(".bg4").css({
         'transform': 'translate(0px, ' + wScroll / 70 + '%)',
         'z-index': 1
     })
