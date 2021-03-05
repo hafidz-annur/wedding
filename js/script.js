c14523cc7b8e7304deca0f61195b613a0d9eb048
annur25 <hafidz.bdt@gmail.com>
2021-03-06 00:08:13 +0700

    Update 05/03/2021

--- js/script.js
+++ js/script.js
@@ -6,25 +6,16 @@ $(window).scroll(function () {
         'z-index': 1
     })
     $(".bg2").css({
-        'transform': 'translate(0px, ' + wScroll / 40 + '%)',
+        'transform': 'translate(0px, ' + wScroll / 25 + '%)',
         'z-index': 1
     })
     $(".bg3").css({
-        'transform': 'translate(0px, ' + wScroll / 60 + '%)',
+        'transform': 'translate(0px, ' + wScroll / 30 + '%)',
         'z-index': 1
     })
     $(".bg4").css({
-        'transform': 'translate(0px, ' + wScroll / 70 + '%)',
+        'transform': 'translate(0px, ' + wScroll / 35 + '%)',
         'z-index': 1
     })
-
-    $(".couple1").css({
-        'transform': 'translate(-' + wScroll / 10 + 'px, ' + '0px)'
-    })
-
-    $("#couple2").css({
-        'transform': 'translate(' + wScroll / 10 + 'px, ' + '0px)'
-    })
-
 
 })
